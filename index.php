ce39f79bd8e392fc424d6fbc40e110ca8ea26ff1
stephenhou <stephen.hou.53@gmail.com>
2016-06-10 00:13:42 -0700

    login page

--- index.php
+++ index.php
@@ -1,34 +1,77 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Gym Tracker</title>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <title>GRIND - Optimize your Workout!</title>
 
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- Bootstrap -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Optional theme -->
-    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+  <!-- Optional theme -->
+  <link href="css/bootstrap-theme.min.css" rel="stylesheet">
 
-    <!-- remember own stylesheet must go here, below bootstrap-->
+  <!-- remember own stylesheet must go here, below bootstrap-->
+  <link href="css/font-awesome.min.css" rel="stylesheet">
+  <link href='https://fonts.googleapis.com/css?family=Cabin' rel='stylesheet' type='text/css'>
+  <link href="css/stylesheet.css" rel="stylesheet">
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-  <p>hello world jasklfkjslkfj</p>
+      <![endif]-->
+    </head>
+    <body>
+      <h1>Always On My Grind</h1>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) and links to other JS files-->
-    <script src="js/jquery.min.js"></script>
+      <div class="col-md-6">
+        <h3 class="lg-txt">Let's build a <br>better you.</h3>
+        <div class="checklist">
+          <i class="fa fa-check"></i>Manage your workouts<br><br>
+          <i class="fa fa-check"></i>Find helpful trainers<br><br>
+          <i class="fa fa-check"></i>Achieve your goals
+        </div> 
+      </div>
+      <div class="col-md-6">
+        <h3 class="lg-txt">Sign Up</h3>
 
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-  </body>
-</html>
+        <form id="regForm" action="submit.php" method="post">
+          
+          <div class="col-md-6 spacing">
+            <input type="text" class="fld" name="fname" value="" placeholder="First Name">
+          </div>
+
+          <div class="col-md-6 spacing">
+            <input type="text" class="fld" name="lname" value="" placeholder="Last Name">
+          </div>
+          <div class="col-md-12 spacing">
+            <input type="text" class="fld" name="email" value="" placeholder="Your email address">
+          </div>
+          <div class="col-md-12 spacing">
+            <input type="text" class="fld" name="username" value="" placeholder="Pick a username">
+          </div>
+          <div class="col-md-12 spacing">
+            <input type="text" class="fld" name="password" value="" placeholder="Create a password">
+          </div>
+          <div class="wrapper">
+            <button type="button" class="btn-lg btn-danger">Sign up!</button>
+          </div>
+
+        </form>
+
+      </div>
+
+
+
+
+      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) and links to other JS files-->
+      <script src="js/jquery.min.js"></script>
+
+      <!-- Include all compiled plugins (below), or include individual files as needed -->
+      <script src="js/bootstrap.min.js"></script>
+    </body>
+    </html>
